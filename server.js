7c77bb14dbb1d5372ec61d297a478dd30bfc8d6b
gavinlai999 <gavinlai07@gmail.com>
2023-06-28 16:01:13 -0700

    fix error 2

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require("express");
+const express = require("express"); 
+// instantiates Express
 const app = express();
+// for serving files in the public folder, such as CSS files
 app.use(express.static("./public"));
+// server responds when the client sends a GET request for the / endpoint
 
-app.get("/", function(req,res){
- HEAD
-  res.sendFile(__dirname + '/index.html');
-
-  res.sendFile(__dirname + "/index.html");
-
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + "/index.html");
 });
-app.post("/", function(req,res){
-  res.sendFile(__dirname + "/index2.html");
+app.post("/", function(req,res) {
+     res.sendFile(__dirname + "/index2.html");
 });
-app.listen(process.env.PORT || 3000, function(){
-  console.log('Server is running on port ${process.env.PORT}.');
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Server is running on port ${process.env.PORT}.`);
 });
